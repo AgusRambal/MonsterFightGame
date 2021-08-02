b4cb1164b105189f11363d6a0691fd256e0107b2
Agustin Rambaldo <agustin.rc97@gmail.com>
2021-08-02 16:38:54 -0300

    Animations and logic
    Agregue animaciones y algunas logicas de funcionamiento menores, sistema de vida preparado y funcionando

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,23 +7,32 @@ public class Enemy : MonoBehaviour
     public int maxHealth;
     int currentHealth;
 
+    public HealthBarScript healthBar;
+    public Animator animator;
+
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage (int damage)
     {
         currentHealth -= damage;
 
+        healthBar.SetHealth(currentHealth);
+
         if (currentHealth <= 0)
         {
             Die();
+
         }
     }
 
     void Die()
     {
-        Debug.Log("x");
+        animator.SetBool("IsDead", true);
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
     }
 }
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -9,11 +9,42 @@ public class GameControl : MonoBehaviour
     private string selectedCharacterDataName = "SelectedCharacter";
     public int selectedCharacter;
     public GameObject player;
+    public GameObject foto1, foto2, foto3, foto4;
     
     void Start()
     {
         selectedCharacter = PlayerPrefs.GetInt(selectedCharacterDataName, 0);
         player = Instantiate(characters[selectedCharacter], playerStartPosition.position, characters[selectedCharacter].transform.rotation);
+
+        if (selectedCharacter == 0)
+        {
+            foto1.SetActive(true);
+            foto2.SetActive(false);
+            foto3.SetActive(false);
+            foto4.SetActive(false);
+        }
+        else if (selectedCharacter == 1)
+        {
+            foto1.SetActive(false);
+            foto2.SetActive(true);
+            foto3.SetActive(false);
+            foto4.SetActive(false);
+        }
+        else if (selectedCharacter == 2)
+        {
+            foto1.SetActive(false);
+            foto2.SetActive(false);
+            foto3.SetActive(true);
+            foto4.SetActive(false);
+        }
+        else if (selectedCharacter == 3)
+        {
+            foto1.SetActive(false);
+            foto2.SetActive(false);
+            foto3.SetActive(false);
+            foto4.SetActive(true);
+        }
+
     }
 }
  
